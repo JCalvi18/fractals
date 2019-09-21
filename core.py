bf34bd1b115e7cc25910d460d4be0fa64ad47b67
Jorge Calvimontes <calvimontesjorges@gmail.com>
2019-09-21 08:36:23 -0400

    migration to mxnet

--- core.py
+++ core.py
@@ -1,110 +1,139 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from multiprocessing import Pool,Lock
 import os
-import torch as t
+import mxnet as mx
+from mxnet import nd
+from time import time
 
 
 class Fractal(object):
-    def __init__(self, resolution=[100, 100], point=[-1.7492199, -0.000335], scale=1.875e-05, nrep=200):
+    def __init__(self, resolution, point, scale=1.875e-05, nrep=200, device=-1, row_wise=True, debug=False):
         self.resolution = resolution
         self.point = point
         self.scale = scale
-        self.width = [1.25*self.scale, 1*self.scale]
+        self.wh = [1.25*self.scale, 1*self.scale]
         self.nrep = nrep
-        self.M = t.Tensor(np.zeros(resolution, dtype=np.int16)).cuda()
+        self.row_wise = row_wise
+        self.debug = debug
+        self.ctx = mx.gpu(device) if device >= 0 else mx.cpu(0)
+        self.M = np.zeros(resolution)
+        if not self.row_wise:
+            self.M = self.M.T
 
-    def get_args(self):
+    def change(self, point, scale):
+        self.point = point
+        self.scale = scale
 
-        x_lim = [self.point[0]-self.width[0], self.point[0]+self.width[0]]
-        y_lim = [self.point[1]+self.width[1], self.point[1]-self.width[1]]
+    def get_row_column(self):
+        # Generate rows or columns of the C plane
+        x_lim = [self.point[0]-self.wh[0], self.point[0]+self.wh[0]]
+        y_lim = [self.point[1]+self.wh[1], self.point[1]-self.wh[1]]
 
-        x0 = np.linspace(x_lim[0], x_lim[1], self.resolution[0], dtype='double')
-        x0 = np.tile(x0, self.resolution[1])
-        y0 = np.linspace(y_lim[1], y_lim[0], self.resolution[1], dtype='double')
-        y0 = np.repeat(y0, self.resolution[0])
-        return zip(x0, y0, range(self.resolution[0]*self.resolution[1]))
+        X = np.linspace(x_lim[0], x_lim[1], self.resolution[0], dtype='double')
+        Y = np.linspace(y_lim[1], y_lim[0], self.resolution[1], dtype='double')
+        if self.row_wise:
+            for y in Y:
+                yield [(x, y) for x in X]
+        else:
+            for x in Y:
+                yield [(x, y) for y in Y]
 
-    def update_matrix(self,args):
-        #args[0]=index
-        #args[1]=val
-        self.M[args[0]]=args[1]
+    def gen(self, frac_type, chunk_size=2):
+        cs = chunk_size
+        chunk = []
+        slices = []
+        start_exec = time()
+        chunk_time = np.array([])
+        for i, r in enumerate(self.get_row_column()):
+            chunk += r
+            slices.append(slice((cs-1)*len(r), cs*len(r)))
+            cs -= 1
+            if cs:
+                continue
+            in_points = nd.array(chunk, ctx=self.ctx).T  # Points to evaluate
+            init_vals = nd.zeros((3, in_points.shape[1]), ctx=self.ctx)  # initial values of r, im and it
+            # Operations performed in GPU
+            start_chunk = time()
+            out_stream = frac_type(in_points, init_vals, self.nrep).asnumpy()
+            if len(slices) > 1:
+                slices.reverse()
+                for j, s in zip(range((i+1)-chunk_size, i+1), slices):
+                    self.M[j] = out_stream[s]
+            else:
+                self.M[i] = out_stream
+            chunk_time = np.append(chunk_time, time()-start_chunk)
 
+            cs = chunk_size
+            slices = []
+            chunk = []
+
+        if self.debug:
+            print('Plane generation:{:0.3f}s'.format(time()-start_exec))
+            print('Average chunk execution:{:0.3f}s'.format(chunk_time.mean()))
 
 class Fractal_GUI(Fractal):
-    def __init__(self,*args,**kwargs):
-        super(Fractal_GUI, self).__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super(Fractal_GUI, self).__init__(*args, **kwargs)
         self.fig, self.ax = plt.subplots()
-        self.mv=4
-        self.point_text=plt.text(0.02, .9, str('Point: '+str(self.point)), fontsize=14, transform=plt.gcf().transFigure)
-        self.scale_text=plt.text(0.02, .8, str('Scale: '+str(self.scale)), fontsize=14, transform=plt.gcf().transFigure)
+        self.mv = 4
+        self.point_text = plt.text(0.02, .9, str('Point: '+str(self.point)), fontsize=14, transform=plt.gcf().transFigure)
+        self.scale_text = plt.text(0.02, .8, str('Scale: '+str(self.scale)), fontsize=14, transform=plt.gcf().transFigure)
 
     def print_text(self):
         self.point_text.remove()
         self.point_text=plt.text(0.02, .9, str('Point: '+str(self.point)), fontsize=14, transform=plt.gcf().transFigure)
         self.scale_text.remove()
         self.scale_text=plt.text(0.02, .8, str('Scale: '+str(self.scale)), fontsize=14, transform=plt.gcf().transFigure)
-    def event_update(self,event):
-        if event.key=='left':
-            self.point[0]-=self.scale/self.mv
-        elif event.key=='right':
-            self.point[0]+=self.scale/self.mv
-        elif event.key=='up':
-            self.point[1]-=self.scale/self.mv
-        elif event.key=='down':
-            self.point[1]+=self.scale/self.mv
-        elif event.key=='z':
-            self.scale/=2
-            self.width=[1.25*self.scale,1*self.scale]
-        elif event.key=='x':
-            self.scale*=2
-            self.width=[1.25*self.scale,1*self.scale]
-        elif event.key=='c':
+
+    def event_update(self, event):
+        if event.key == 'left':
+            self.point[0] -= self.scale/self.mv
+        elif event.key == 'right':
+            self.point[0] += self.scale/self.mv
+        elif event.key == 'up':
+            self.point[1] -= self.scale/self.mv
+        elif event.key == 'down':
+            self.point[1] += self.scale/self.mv
+        elif event.key == 'z':
+            self.scale /= 2
+            self.wh = [1.25*self.scale, 1*self.scale]
+        elif event.key == 'x':
+            self.scale *= 2
+            self.wh = [1.25*self.scale, 1*self.scale]
+        elif event.key == 'c':
             self.copy()
-        elif event.key=='escape':
+        elif event.key == 'escape':
             return 0
         self.print_text()
 
         return 1
 
     def show(self):
-        self.ax.imshow(self.M.reshape(self.resolution),interpolation='nearest')
+        self.ax.imshow(self.M, interpolation='nearest')
         plt.draw()
 
     def copy(self):
-        s=str('python server.py -px '+str(self.point[0])+' -py '+str(self.point[1])+' -s '+str(self.scale))
-        os.system('echo %s |xclip -selection c'%s)
+        s = '"frac.change([{:0.5f}, {:0.5f}], {})"'.format(self.point[0], self.point[1], self.scale)
+        os.system('echo {} |xclip -selection c'.format(s))
 
 
-class Artist_GUI(object):
-    """docstring for Artist_GUI"""
-    def __init__(self, arg):
-        super(Artist_GUI, self).__init__()
-        self.arg = arg
-
-
-def mandelbrot(args):
-    x,y,i,nrep=args
-    r=im=it=0
-    while r**2 + im**2 < 4  and  it < nrep:
+def mandelbrot(points, init, nrep):
+    x, y = points
+    r, im, it = init
+    for _ in range(nrep):
+        mag = r**2 + im**2
         aux = r**2 - im**2 + x
         im = 2*r*im + y
         r = aux
-        it+=1
-    return i,it
-
-
-def gen(frac_type, frac_class):
-    p = Pool()
-    for x,y,i in frac_class.get_args():
-        res=p.apply_async(frac_type, args=((x,y,i,frac_class.nrep),),callback=frac_class.update_matrix)
-    p.close()
-    p.join()
+        if all(mag >= 4):
+            break
+        it += mag < 4
+    return it
 
 
 def event_handler(event, frac):
     if frac.event_update(event):
-        gen(mandelbrot, frac)
+        frac.gen(mandelbrot)
         frac.show()
     else:
         plt.close()
